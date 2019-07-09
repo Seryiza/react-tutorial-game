719180621b6e1405583e0465a0c2ed8555ab6ff1
Sergey Zaborovsky <seryiza@yandex.ru>
2019-07-10 10:27:30 +1100

    Завершение создания TTT: игра работает и выводит победителя, если он есть

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ class Board extends React.Component {
 
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
@@ -32,12 +33,26 @@ class Board extends React.Component {
 
     handleClick(i) {
         let squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     render() {
-        const status = 'Next player: X';
+        let status;
+        const winner = calculateWinner(this.state.squares);
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -76,6 +91,28 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+// ========================================
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
